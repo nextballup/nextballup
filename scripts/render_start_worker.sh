12f6594e06100e1c948341ce095090f88d85dc1d
Kymi808 <zeng.kyle13@gmail.com>
2026-05-06 01:50:30 -0500

    Constrain alpha worker concurrency

--- scripts/render_start_worker.sh
+++ scripts/render_start_worker.sh
@@ -3,4 +3,6 @@ set -eu
 
 exec celery -A nextballup_worker.celery_app worker \
   --loglevel=info \
+  --concurrency="${CELERY_WORKER_CONCURRENCY:-1}" \
+  --max-tasks-per-child="${CELERY_WORKER_MAX_TASKS_PER_CHILD:-5}" \
   --queues=nextballup.default,nextballup.transcode,nextballup.maintenance,nextballup.cpu
